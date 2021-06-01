dad927253ce6458d0c080e91ac4dfe76775d108e
Theo Oliveira <theophilo.oliveira@gmail.com.br>
2021-05-31 23:21:50 -0300

    initial commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function RepositoryList() {
+    return (
+        <section className="repo-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositório</a>
+                </li>
+
+            </ul>
+        </section>
+    )
+}
