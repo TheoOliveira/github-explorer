b3e65a2eeeb980dfc046e6c9dfb4170ff3754749
Theo Oliveira <theophilo.oliveira@gmail.com.br>
2021-06-06 03:05:51 -0300

    fast refresh | fetch e listagem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import RepositoryItem from './RepositoryItem'
+import React, { useState, useEffect } from "react";
+import RepositoryItem from "./RepositoryItem";
+import "../styles/repositories.scss";
 
 const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/unform/unform",
+};
 export default function RepositoryList() {
+  const [repolist, setRepolist] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+    .then((res) =>
+      res.json()
+    ).then(data => setRepolist(data))
+  }, [repolist]);
   return (
     <section className="repo-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-          <RepositoryItem repository={repository} />
-          <RepositoryItem repository={repository} />
-          <RepositoryItem repository={repository} />
-
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
